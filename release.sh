dd9d2f7c350f3f8eca279886355a54591cbd4a98
tanren <hello@tanren.dev>
2026-08-15 14:11:54 +0200

    ci: add commitizen release script

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu
+
+uvx --from commitizen cz bump --yes
+
+version=$(sed -n 's/^version = "\(.*\)"/\1/p' patina/plugin.toml)
+rev=$(git rev-parse HEAD)
+api=$(sed -n 's/^plugin_api = \([0-9]*\)/\1/p' patina/plugin.toml)
+row=$(printf '[[plugin.release]]\nplugin_api = %s\nversion = "%s"\nrev = "%s"' "$api" "$version" "$rev")
+awk -v row="$row" '
+  !done && /^\[\[plugin\.release\]\]/ { print row; print ""; done = 1 }
+  { print }
+  END { if (!done) { print ""; print row } }
+' catalog.toml > catalog.toml.new
+mv catalog.toml.new catalog.toml
+git commit -am "chore: add $version release row"
+
+git push origin HEAD --tags
