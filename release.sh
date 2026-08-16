f87b1364673b64ff8d59f9e5b62998029c7546b8
tanren <hello@tanren.dev>
2026-08-16 20:53:59 +0200

    fix: publish plugin version in catalog

--- release.sh
+++ release.sh
@@ -6,6 +6,7 @@ uvx --from commitizen cz bump --yes
 version=$(sed -n 's/^version = "\(.*\)"/\1/p' patina/plugin.toml)
 rev=$(git rev-parse HEAD)
 api=$(sed -n 's/^plugin_api = \([0-9]*\)/\1/p' patina/plugin.toml)
+sed -i "0,/^version = /s/^version = .*/version = \"$version\"/" catalog.toml
 row=$(printf '[[plugin.release]]\nplugin_api = %s\nversion = "%s"\nrev = "%s"' "$api" "$version" "$rev")
 awk -v row="$row" '
   !done && /^\[\[plugin\.release\]\]/ { print row; print ""; done = 1 }
